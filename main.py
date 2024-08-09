37f7490d0d7c01ba580c1c57df9ea298d51ca077
Evangelos Stylianos Softsis <vag03so@gmail.com>
2024-08-09 15:12:02 +0300

    Checks the file formats in the given audio folder

--- main.py
+++ main.py
@@ -22,110 +22,118 @@ from moviepy.video.VideoClip import ImageClip
 
 # Prompts the user to select a file and returns its path
 def select_file(image_input):
+    # Prompts the user
     Tk().withdraw()
     file_path = askopenfilename()
+    # Clears the entry box
     image_input.delete(0, tk.END)
+    # Inserts the chosen path to the entry box
     image_input.insert(0, file_path)
 
 # Prompts the user to select a folder and returns its path
 def select_folder(audio_input):
+    # Prompts the user
     Tk().withdraw()
     folder_path = askdirectory()
+    # Clears the entry box
     audio_input.delete(0, tk.END)
+    # Inserts the chosen path to the entry box
     audio_input.insert(0, folder_path)
 
 # Checks the validity of the user's inputs
 def check_settings(root):
+    # Error msg to be shown to the user if sth goes wrong
     error_msg = ''
 
     # Checks image file path input
     try:
         with Image.open(root.nametowidget("image_frame").nametowidget('image_input').get()) as img:
             img.verify()
-            print('Image file good')
     except (IOError, SyntaxError):
         error_msg += 'Invalid image file\n'
 
-    if os.path.isdir(root.children.get('audio_frame').nametowidget('audio_input').get()):
-        print('Audio folder good')
-    else:
+    # Checks audio folder path input
+    if not os.path.isdir(root.children.get('audio_frame').nametowidget('audio_input').get()):
         error_msg += 'Invalid audio folder\n'
 
-    if os.path.isdir(root.children.get('save_frame').nametowidget('save_input').get()):
-        print('Save folder good')
-    else:
+    # Checks save folder path input
+    if not os.path.isdir(root.children.get('save_frame').nametowidget('save_input').get()):
         error_msg += 'Invalid save folder\n'
 
 
-
+    # Checks the resolution input
     width = root.children.get('vidsets_frame').nametowidget('width_input').get()
     height = root.children.get('vidsets_frame').nametowidget('height_input').get()
     if width == '' and height == '':
-        print('Resolution good')
+        pass
     elif width != '' and height != '' and width.isdigit() and height.isdigit() and int(width) > 0 and int(height) > 0:
-        print('Resolution good')
+        pass
     else:
         error_msg += 'Invalid resolution\n'
 
 
-
+    # Checks the fps input
     fps = root.children.get('vidsets_frame').nametowidget('fps_input').get()
-    if fps != '' and fps.isdigit() and int(fps) > 0:
-        print('FPS good')
-    else:
+    if not (fps != '' and fps.isdigit() and int(fps) > 0):
         error_msg += 'Invalid FPS\n'
 
 
+    # Checks the audio fade-in input
     fade_in = root.children.get('audiosets_frame').nametowidget('audiofadein_input').get()
     if fade_in == '':
-        print('Fade-In good')
+        pass
     elif fade_in.isdigit() and int(fade_in) > 0:
-        print('Fade-In good')
+        pass
     else:
         error_msg += 'Invalid fade-in seconds\n'
 
+    # Checks the audio fade-out input
     fade_out = root.children.get('audiosets_frame').nametowidget('audiofadeout_input').get()
     if fade_out == '':
-        print('Fade-In good')
+        pass
     elif fade_out.isdigit() and int(fade_out) > 0:
-        print('Fade-In good')
+        pass
     else:
         error_msg += 'Invalid fade-out seconds\n'
 
 
+    # Checks the hours input
     hours = root.children.get('extrasets_frame').nametowidget('hours_input').get()
     if hours == '':
-        print('Hours good')
+        pass
     elif hours.isdigit() and int(hours) > 0:
-        print('Hours good')
+        pass
     else:
         error_msg += 'Invalid hours\n'
 
 
+    # Checks the mins input
     mins = root.children.get('extrasets_frame').nametowidget('mins_input').get()
     if mins == '':
-        print('Mins good')
+        pass
     elif mins.isdigit() and int(mins) > 0:
-        print('Mins good')
+        pass
     else:
         error_msg += 'Invalid mins\n'
 
 
+    # Checks the secs input
     secs = root.children.get('extrasets_frame').nametowidget('secs_input').get()
     if secs == '':
-        print('Secs good')
+        pass
     elif secs.isdigit() and int(secs) > 0:
-        print('Secs good')
+        pass
     else:
         error_msg += 'Invalid secs\n'
 
 
-    if is_valid_filename(root.children.get('export_frame').nametowidget('export_input').get()):
-        print('Save file good')
-    else:
+
+    # Checks the save file path input
+    if not is_valid_filename(root.children.get('export_frame').nametowidget('export_input').get()):
         error_msg += 'Invalid save file name\n'
 
 
+    # Shows an error message to the user if any input was invalid
     if error_msg != '':
         tk.messagebox.showerror(title='Invalid Settings', message=error_msg)
         return False
@@ -155,7 +163,6 @@ def save_settings(root):
     with open('settings.json', 'w') as file:
         json.dump(settings_dict, file)
 
-    print("Settings saved successfully.")
     return settings_dict
 
 # Loads the settings
@@ -164,7 +171,6 @@ def load_settings():
     try:
         with open('settings.json', 'r') as file:
             settings_dict = json.load(file)
-        print("Settings loaded successfully.")
         return settings_dict
     except FileNotFoundError:
         print("No settings file found.")
@@ -175,88 +181,108 @@ def load_settings():
 
 
 def get_audio_clips(settings):
-    audio_files = os.listdir(settings.get('audio_folder'))
-    audio_clips = []
-    total_duration = 0
-    for audio in audio_files:
-        audio_clip = AudioFileClip(settings.get('audio_folder') + '/' + audio)
-        total_duration += audio_clip.duration
+    # Gets all the audio files from the given folder and ignored the other types of files
+    audio_files = []
+    for file in os.listdir(settings.get('audio_folder')):
+        if file.endswith('.mp3') or file.endswith('.wav') or file.endswith('.aac') or file.endswith(
+                '.flac') or file.endswith('.m4a') or file.endswith('.m4b') or file.endswith(
+                '.wma') or file.endswith('.aiff') or file.endswith('.ogg'):
+            audio_files.append(file)
+        else:
+            print('Invalid file format found')
 
-        if settings.get('fade_in_secs').isdigit():
-            audio_clip = audio_clip.fx(audio_fadein, int(settings.get('fade_in_secs')))
+    # Checks if there was at least one audio file
+    if len(audio_files) != 0:
+        audio_clips = []
+        total_duration = 0
+        for audio in audio_files:
+            # Creates an AudioFileClip
+            audio_clip = AudioFileClip(settings.get('audio_folder') + '/' + audio)
+            # Adds the duration
+            total_duration += audio_clip.duration
 
-        if settings.get('fade_out_secs').isdigit():
-            audio_clip = audio_clip.fx(audio_fadeout, int(settings.get('fade_out_secs')))
+            # Adds fade-in/fade-out effects to the audio
+            if settings.get('fade_in_secs').isdigit():
+                audio_clip = audio_clip.fx(audio_fadein, int(settings.get('fade_in_secs')))
+            if settings.get('fade_out_secs').isdigit():
+                audio_clip = audio_clip.fx(audio_fadeout, int(settings.get('fade_out_secs')))
 
-        audio_clips.append(audio_clip)
+            audio_clips.append(audio_clip)
 
-    mixxed_audio = concatenate_audioclips(audio_clips)
+        # Mixxes all the audio clips together
+        mixxed_audio = concatenate_audioclips(audio_clips)
 
-    return mixxed_audio, round(total_duration)
-
+        # Returns the mixxed clips and their total duration
+        return mixxed_audio, round(total_duration)
+    # Shows an error msg if there were 0 audio files
+    else:
+        tk.messagebox.showerror(title='Audio files error', message='0 audio files found')
+        return None, 0
 
 
 # Exports the video
 def export_video(root):
+    # Checks the validity of the given settings
     if check_settings(root):
+        # Saves the given settings for the next session
         settings = save_settings(root)
 
+        # Calculates the max duration of each part if given
         max_duration = 0
-        if settings.get('max_hours').isdigit():
+        if settings.get('max_hours') != '':
             max_duration += int(settings.get('max_hours'))*60*60
-        else:
-            print('Hours empty or invalid')
-
-        if settings.get('max_mins').isdigit():
+        if settings.get('max_mins') != '':
             max_duration += int(settings.get('max_mins'))*60
-        else:
-            print('Mins empty or invalid')
-
-        if settings.get('max_secs').isdigit():
+        if settings.get('max_secs') != '':
             max_duration += int(settings.get('max_secs'))
-        else:
-            print('Secs empty or invalid')
 
+        # Gets the audio for the video and its duration
         mixxed_audio, total_duration = get_audio_clips(settings)
+        if mixxed_audio != None:
+            final_clips = []
+            try:
+                # Creates image clip with the given image
+                image_clip = ImageClip(settings.get('image_file'))
 
-        final_clips = []
-        try:
-            image_clip = ImageClip(settings.get('image_file'))
+                # Mixxes the image clip with the audio
+                mixxed_clip = image_clip.set_audio(mixxed_audio).set_duration(total_duration)
 
-            mixxed_clip = image_clip.set_audio(mixxed_audio).set_duration(total_duration)
+                # Cuts the video into seperate parts if a max duration was given
+                if max_duration > 0:
+                    starting_point = 0
+                    while total_duration > max_duration:
+                        final_clips.append(mixxed_clip.subclip(starting_point, starting_point + max_duration))
+                        starting_point += max_duration - 1
+                        total_duration -= max_duration
 
-            if max_duration > 0:
-                starting_point = 0
-                while total_duration > max_duration:
-                    final_clips.append(mixxed_clip.subclip(starting_point, starting_point + max_duration))
-                    starting_point += max_duration - 1
-                    total_duration -= max_duration
+                    final_clips.append(mixxed_clip.subclip(starting_point, starting_point + total_duration))
+                else:
+                    final_clips.append(mixxed_clip)
 
-                final_clips.append(mixxed_clip.subclip(starting_point, starting_point + total_duration))
-            else:
-                final_clips.append(mixxed_clip)
-
-        except ValueError:
-            print('Problem with the image path')
-            return
+            except ValueError:
+                print('Problem with the image path')
+                return
 
 
+            # Exports the final video
+            try:
+                # If there are seperate parts, a clip number is added to the file name
+                if len(final_clips) != 1:
+                    clip_number = 1
+                    for final_clip in final_clips:
+                        save_file_name = settings.get('save_folder') + '/' + settings.get('file_name') + str(clip_number) + '.mp4'
+                        final_clip.write_videofile(save_file_name, codec='libx264', audio_bitrate='3000k', fps=int(settings.get('fps')))
+                        clip_number += 1
+                else:
+                    save_file_name = settings.get('save_folder') + '/' + settings.get('file_name') + '.mp4'
 
-        try:
-            if len(final_clips) != 1:
-                clip_number = 1
-                for final_clip in final_clips:
-                    save_file_name = settings.get('save_folder') + '/' + settings.get('file_name') + str(clip_number) + '.mp4'
-                    final_clip.write_videofile(save_file_name, codec = 'libx264', audio_bitrate = '3000k', fps = int(settings.get('fps')))
-                    clip_number += 1
-            else:
-                save_file_name = settings.get('save_folder') + '/' + settings.get('file_name') + '.mp4'
+                    final_clips[0].write_videofile(save_file_name, codec='libx264', audio_bitrate='3000k', fps=int(settings.get('fps')))
 
-                final_clips[0].write_videofile(save_file_name, codec='libx264', audio_bitrate='3000k', fps=int(settings.get('fps')))
-
-        except ValueError:
-            print('Problem with the save path')
-            return
+            except ValueError:
+                print('Problem with the save path')
+                return
+        else:
+            return False
     else:
         return False
 
@@ -437,6 +463,4 @@ if __name__ == "__main__":
 
 # TODO: Audio normalizing
 # TODO: Tooltips
-# TODO: Save file name
 # TODO: Codecs and bitrates
-# TODO: Check audio files in folder
